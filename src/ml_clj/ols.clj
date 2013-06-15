55880f161b4dd3ad493b6a490a4380b4ffb4bde4
Ian Clark <idclark13@gmail.com>
2013-06-15 18:04:53 -0400

    fixes Incanter namespace bug between stats and core

--- src/ml_clj/ols.clj
+++ src/ml_clj/ols.clj
@@ -8,24 +8,25 @@
 linear regression for a continous dependent variable and a set of regressors.
 
 Input: vector for the independent variable
-       matrix of regressors. Intercept defaults to true, 
+       matrix of regressors. Intercept defaults to true.
+
 
 
 "
   [y-vec x-mat & {:keys [intercept] :or {intercept true}}]
-  (let [x (if intercept (core/bind-coumns (replicate (nrow x) 1) x)x)
-        x'xI  (stats/solve (core/mmult (core/trans x ) x))
-        x'y (core/mmult (core/trans x) y)
+  (let [x (if intercept (core/bind-columns (replicate (core/nrow x-mat) 1) x-mat) x-mat)
+        x'xI  (core/solve (core/mmult (core/trans x ) x))
+        x'y (core/mmult (core/trans x) y-vec)
         coefs (* x'xI x'y)
-        fit (to-list (if number? coefs)
-                     (mult x coefs)
-                     (mmult x coefs))
-        resids (to-list (core/minus y fit))
+        fit (core/to-list (if number? coefs)
+                     (core/mult x coefs)
+                     (core/mmult x coefs))
+        resids (core/to-list (core/minus y-vec fit))
         sse (core/sum-of-squares resids)
-        ssr (core/sum-of-squares (core/minus fit) (core/mean fit))
+        ssr (core/sum-of-squares (core/minus fit) (stats/mean fit))
         sst (+ sse ssr)
         r-square (core/safe-div ssr sst)
-        n (core/nrow y)
+        n (core/nrow y-vec)
         p (core/ncol x)
         df (- n p)
         p-1 (if intercept (dec p) p)
@@ -33,18 +34,18 @@ Input: vector for the independent variable
         covar-mat (core/mult mse x'xI)
         std-errors (core/sqrt (core/diag covar-mat))
         t-score (/ coefs std-errors)
-        95-conf (core/mult (core/quantile-t 0.975 :df df2 ) std-errors)
+        conf-95 (core/mult (stats/quantile-t 0.975 :df df ) std-errors)
         coefs-CI (if (number? std-errors)
-                   [(core/plus coefs 95-conf)
-                    (core/minus coefs 95-conf)]
+                   [(core/plus coefs conf-95)
+                    (core/minus coefs conf-95)]
                     (partition 2
                                (interleave
-                                (core/minus coefs 95-conf)
-                                (core/plus coefs 95-conf))))
+                                (core/minus coefs conf-95)
+                                (core/plus coefs conf-95))))
         ]
     (with-meta
       {:x x
-       :y y
+       :y y-vec
        :fit fit
        :coefs coefs
        :std-errors std-errors
