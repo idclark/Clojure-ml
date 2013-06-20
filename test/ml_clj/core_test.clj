5d1ca72ca1fbc5b26b2fe8a85fd55d0aea62a466
Ian Clark <idclark13@gmail.com>
2013-06-19 22:59:18 -0400

    linear regression test passes

--- test/ml_clj/core_test.clj
+++ test/ml_clj/core_test.clj
@@ -1,7 +1,18 @@
 (ns ml-clj.core-test
   (:use clojure.test
-        ml-clj.core))
+        ml-clj.ols
+        incanter.core
+        incanter.stats
+        incanter.datasets))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def iris (to-matrix (get-dataset :iris) :dummies true))
+  (def y (sel iris :cols 0))
+  (def x (sel iris :cols (range 1 6)))
+  (def iris-lm (linear-model y x))
+
+(def incanter-lm (linear-model y x))
+(def ian-lm (ols y x))
+
+(deftest coef-test
+  (testing "coefs"
+    (= (:coefs incanter-lm) (:coefs ian-lm))))
