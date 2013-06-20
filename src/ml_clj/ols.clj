1b205b01cab44a44dd931d659227746ebc0b0526
Ian Clark <idclark13@gmail.com>
2013-06-19 22:52:38 -0400

    huge rewrite using incanter notation and logic

--- src/ml_clj/ols.clj
+++ src/ml_clj/ols.clj
@@ -1,7 +1,9 @@
 (ns ml-clj.ols
-(:require [incanter.core :as core]
-         [incanter.stats :as stats]
-         [incanter.datasets :as datasets]))
+(:use [incanter.core :only ( abs plus minus div mult mmult to-list bind-columns
+                               sqrt diag trans  ncol
+                              nrow matrix length  sum sum-of-squares sel solve
+                              bind-rows safe-div)]
+      [incanter.stats :only (mean quantile-t cdf-f cdf-t)]))
 
 (defn ols
 "
@@ -10,54 +12,75 @@ linear regression for a continous dependent variable and a set of regressors.
 Input: vector for the independent variable
        matrix of regressors. Intercept defaults to true.
 
-
+intercept is not included at the moment!
 
 "
-  [y-vec x-mat & {:keys [intercept] :or {intercept true}}]
-  (let [x (if intercept (core/bind-columns (replicate (core/nrow x-mat) 1) x-mat) x-mat)
-        x'xI  (core/solve (core/mmult (core/trans x ) x))
-        x'y (core/mmult (core/trans x) y-vec)
-        coefs (core/mmult x'xI x'y)
-        fit (core/to-list (if number? coefs)
-                     (core/mult x coefs)
-                     (core/mmult x coefs))
-        resids (core/to-list (core/minus y-vec fit))
-        sse (core/sum-of-squares resids)
-        ssr (core/sum-of-squares (core/minus fit (stats/mean fit))
-        sst (+ sse ssr)
-        r-square (core/safe-div ssr sst)
-        n (core/nrow y-vec)
-        p (core/ncol x)
-        df (- n p)
-        p-1 (if intercept (dec p) p)
-        mse (core/safe-div sse (- n p))
-        covar-mat (core/mult mse x'xI)
-        std-errors (core/sqrt (core/diag covar-mat))
-        t-score (/ coefs std-errors)
-        conf-95 (core/mult (stats/quantile-t 0.975 :df df ) std-errors)
-        coefs-CI (if (number? std-errors)
-                   [(core/plus coefs conf-95)
-                    (core/minus coefs conf-95)]
-                    (partition 2
-                               (interleave
-                                (core/minus coefs conf-95)
-                                (core/plus coefs conf-95))))
-        ]
-    (with-meta
-      {:x x
-       :y y-vec
-       :fit fit
-       :coefs coefs
-       :std-errors std-errors
-       :t-score t-score
-       :n n
-       :p p
-       :df df
-       :p-1 p-1
-       :coefs-CI coefs-CI
-       :r-square r-square
-       }
-      {:type ::linear-regression})))
+([y x & {:keys [intercept] :or {intercept true}}]
+    (let [_x (if intercept (bind-columns (replicate (nrow x) 1) x) x)
+          xtx (mmult (trans _x) _x)
+          xtxi (if (number? xtx) (/ 1 xtx) (solve xtx))
+          xty (mmult (trans _x) y)
+          coefs (if (and (number? xtxi) (number? xty))
+                  (* xtxi xty)
+                  (to-list (if (or (number? xtxi) (number? xty))
+                    (mult xtxi xty)
+                    (mmult xtxi xty))))
+          fitted (to-list (if (number? coefs)
+                  (mult _x coefs)
+                  (mmult _x coefs)))
+          resid (to-list (minus y fitted))
+          sse (sum-of-squares resid)
+          ssr (sum-of-squares (minus fitted (mean fitted)))
+          sst (+ sse ssr)
+          r-square (safe-div ssr sst)
+          n (nrow y)
+          p (ncol _x)
+          p-1 (if intercept (dec p) p)
+          adj-r-square (- 1 (* (- 1 r-square) (/ (dec 1) (- n p 1))))
+          mse (safe-div sse (- n p))
+          msr (safe-div ssr p-1)
+          f-stat (safe-div msr mse)
+          df1 p-1
+          df2 (- n p)
+          f-prob (cdf-f f-stat :df1 df1 :df2 df2 :lower-tail? false)
+          coef-var (mult mse xtxi)
+          std-errors (sqrt (diag coef-var))
+          t-tests (div coefs std-errors)
+          t-probs (mult 2 (cdf-t (abs t-tests) :df df2 :lower-tail? false))
+          t-95 (mult (quantile-t 0.975 :df df2) std-errors)
+          coefs-ci (if (number? std-errors)
+                       [(plus coefs t-95)
+                        (minus coefs t-95)]
+                       (partition 2
+                         (interleave
+                           (minus coefs t-95)
+                           (plus coefs t-95))))
+         ]
+      (with-meta
+        {:x _x
+         :y y
+         :fitted fitted
+         :design-matrix _x
+         :coefs coefs
+         :t-tests t-tests
+         :t-probs t-probs
+         :coefs-ci coefs-ci
+         :residuals resid
+         :std-errors std-errors
+         :sse sse
+         :ssr ssr
+         :sst sst
+         :mse mse
+         :msr msr
+         :f-stat f-stat
+         :f-prob f-prob
+         :df [df1 df2]
+         :coef-var coef-var
+         :r-square r-square
+         :adj-r-square adj-r-square
+        }
+        {:type ::linear-model}))))
+ 
 
 
 
