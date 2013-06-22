bff62304f189cb38ee5ba89bbbcd2e494d1dc46e
Ian Clark <idclark13@gmail.com>
2013-06-21 21:55:02 -0400

    refactor for midje unit test, values test passes

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [incanter/incanter "1.5.1"]
-                 ])
+                 ]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  
+
                                   
 
 
---
+++ test/ml_clj/ols_test.clj
@@ -0,0 +1,20 @@
+(ns ml-clj.ols-test
+  (:use midje.sweet
+        ml-clj.ols
+        incanter.core
+        incanter.stats
+        incanter.datasets))
+
+(def iris (to-matrix (get-dataset :iris) :dummies true))
+(def y (sel iris :cols 0))
+(def x (sel iris :cols (range 1 6)))
+(def iris-lm (linear-model y x))
+
+(def incanter-lm (linear-model y x))
+(def ian-lm (ols y x))
+
+;refactor for midje unit test, compare my map to incanter
+(fact
+ "the vals from ian's regression are the same as incanter"
+ (vals incanter-lm) => (vals ian-lm))
+
